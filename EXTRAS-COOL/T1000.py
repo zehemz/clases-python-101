5cde2d5ce3985c4f26005fb66663bf2749862537
Lucas Bais <lucas.bais@gmail.com>
2016-09-08 23:01:19 -0300

    T1000

---
+++ EXTRAS-COOL/T1000.py
@@ -0,0 +1,23 @@
+import hashlib
+
+def handShakeDecode(fullDocument):
+    elementsString = fullDocument.split(";")
+    robots = []
+    for element in elementsString:
+        aux = element.strip("[]").split(",")
+        robots.append({"name": aux[0], "key": aux[2]})
+    return robots
+
+if __name__ == "__main__":
+
+    mFile = open("handshakes.lst", "r")
+    robots = handShakeDecode(mFile.read())
+    mFile.close()
+
+    fileWords = open("Spanish.dic", "r")
+    for line in fileWords:
+        coded = hashlib.sha1(line.replace('\n', '').encode("utf-8")).hexdigest()
+        for element in robots:
+            if coded in element["key"]:
+                element["decoded"] = line
+                print(element["name"]+"-"+element["decoded"])
