8c6af2277461e34c51a050aceff102499bf3e0aa
Lucas Bais <lucas.bais@gmail.com>
2016-09-09 21:57:03 -0300

    extras

---
+++ EXTRAS-COOL/message.py
@@ -0,0 +1,13 @@
+## python -m pip install Pillow
+from PIL import Image
+
+if __name__ == "__main__":
+    picture = Image.open("message2.png")
+    pixelPossibleValues = [ (x,y) for x in range(picture.width) for y in range(picture.height)]
+
+    for x,y in pixelPossibleValues:
+       r,g,b = picture.getpixel( (x,y) )
+       if not(r == 0x00) or not(g == 0x00) or not(b == 0x00):
+           picture.putpixel( (x,y), (0xFF, 0xFF, 0xFF))
+
+    picture.show()
