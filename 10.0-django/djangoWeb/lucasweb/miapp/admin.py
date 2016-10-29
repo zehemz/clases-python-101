2507ef883d360139cfd047deb9fb9b3d7dfe7aab
Lucas Bais <lucas.bais@gmail.com>
2016-10-28 22:27:14 -0300

    pools

--- 10.0-django/djangoWeb/lucasweb/miapp/admin.py
+++ 10.0-django/djangoWeb/lucasweb/miapp/admin.py
@@ -4,8 +4,23 @@ from .models import Question
 from .models import Choice
 from .models import Contact
 
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
 admin.site.register(Contact)
 
 # Register your models here.
